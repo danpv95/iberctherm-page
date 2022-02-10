1e27e8efda8dc48c8d112d07b5339cd269bbba9d
danpv95 <danielpava95@utp.edu.co>
2022-02-10 08:55:51 -0500

    footer and header

--- src/app/website/components/header/header.component.ts
+++ src/app/website/components/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
+
 export class HeaderComponent implements OnInit {
  
   langs: string[] = [];
