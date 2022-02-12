3b4f954ec9fb6448931fe8872e82814141d80509
Diana <dianac.salazarg@gmail.com>
2022-02-12 13:22:55 -0500

    se intenta modificar login

--- src/app/website/pages/ingreso/ingreso.component.ts
+++ src/app/website/pages/ingreso/ingreso.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingreso.component.css'],
 })
 export class IngresoComponent implements OnInit {
-  constructor() {}
+  constructor() {
+        //Se implementa para darle estilos a la vista y la imagen de fondo
+        let $page = document.getElementsByClassName("full-page")[0];
+        let image_src;
+        var image_container = document.createElement("div");
+        image_container.classList.add("full-page-background");
+        $page.classList.add("register-page");
+        image_container.style.backgroundImage = "url(assets/img/fondo2.jpg)";
+        $page.appendChild(image_container);
+  }
 
   ngOnInit(): void {}
 }
--- src/app/website/pages/registro/registro.component.ts
+++ src/app/website/pages/registro/registro.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+      
+   }
 
   ngOnInit(): void {
   }
