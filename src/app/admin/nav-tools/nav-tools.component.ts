77e4dc83eea8f62da99296d2943e5ac62ac983a7
danpv95 <danielpava95@utp.edu.co>
2022-02-14 20:41:42 -0500

    admin

--- src/app/admin/nav-tools/nav-tools.component.ts
+++ src/app/admin/nav-tools/nav-tools.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/core/services/firebase/firebase.service';
 
 @Component({
   selector: 'app-nav-tools',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-tools.component.css']
 })
 export class NavToolsComponent implements OnInit {
-
-  constructor() { }
+  register = [];
+  constructor(public firebaseService: FirebaseService) { }
 
   ngOnInit(): void {
+    this.getRegisters();
+  }
+
+  getRegisters (){
+    this.firebaseService.getRegister().subscribe((register) => {
+      console.log(register);
+      this.register = register;
+    });
+  }
+
+  deleteProduct(event, register) {
+    console.log(register);
+    if (confirm('Are you sure you want to delete if ?')) {
+      this.firebaseService.deleteRegister(register);
+      this.getRegisters();
+    }
   }
 
 }
