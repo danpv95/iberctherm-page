d19778f21e6d1e95319e45c23b8f513714fb8840
danpv95 <danielpava95@utp.edu.co>
2022-03-05 09:31:47 -0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA2z4FdGt6WnANAX9G857eFoi8rEbgWmPs',
+    authDomain: 'iberctherm.firebaseapp.com',
+    projectId: 'iberctherm',
+    storageBucket: 'iberctherm.appspot.com',
+    messagingSenderId: '966101544711',
+    appId: '1:966101544711:web:1bc6f5a23c5787cc185fa2',
+  },
 };
